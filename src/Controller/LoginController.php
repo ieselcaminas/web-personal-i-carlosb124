385b0ba13f4c30442dc181d232b42d909f66f988
carlosb124 <carbuc2@alu.edu.gva.es>
2025-11-11 09:31:37 +0000

    Proyecto entero de Proyecto-Cinema

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // obtener el último error de login
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // obtener el último nombre de usuario ingresado
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
